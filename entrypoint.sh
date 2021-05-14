95ab1f57396e31e13eb8700f65be1c2a1a9e70dd
Tikhanovich Aleksej <aleksej@taotesting.com>
2021-05-14 17:52:19 +0300

    chore: test build

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,42 @@ cp /getExitCode.php /github/workspace/getExitCode.php
 cp /getOutput.php /github/workspace/getOutput.php
 
 echo "Prepare NPM ..."
-npm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN}
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
-cat ~/.npmrc
 
-exit 1
+npm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN}
+
+echo "Prepare composer file ..."
+
+COMPOSER_GENERATION_OUTPUT=$(php generateComposerFile.php)
+echo "$COMPOSER_GENERATION_OUTPUT"
+
+composer config -g github-oauth.github.com $INPUT_GITHUB_TOKEN
+COMPOSER=composer-release.json composer install --no-dev --no-interaction --prefer-source
+
+echo "Prepare Tao to release ..."
+
+mkdir -p taoQtiItem/views/js/mathjax/
+mkdir -p tao/views/locales/en-US/
+mkdir -p config/
+touch taoQtiItem/views/js/mathjax/MathJax.js
+touch index.php
+echo $MESSAGES_JSON > tao/views/locales/en-US/messages.json
+cd $EXT_ID
+git config --global user.name github-actions
+git config --global user.email github-actions@github.com
+git config --global url."https://github.com/".insteadOf git@github.com:
+git config --global url."https://".insteadOf git://
+git config --global url."https://github.com/".insteadOf ssh://git@github.com/
+git remote set-url origin https://${GITHUB_TOKEN}@github.com/$REPO_NAME.git
+git remote set-url --push origin https://${GITHUB_TOKEN}@github.com/$REPO_NAME.git
+git checkout .
+cd ..
+echo "Release extension $EXT_ID"
+
+TAO_RELEASE_OUTPUT=$(taoRelease extensionRelease --extension-to-release ${EXT_ID} --no-interactive --no-write)
+echo "$TAO_RELEASE_OUTPUT"
+
+EXIT_CODE=$(php getExitCode.php $TAO_RELEASE_OUTPUT)
+OUTPUT_MESSAGE=$(php getOutput.php $TAO_RELEASE_OUTPUT)
+
+echo "::set-output name=release_result::$OUTPUT_MESSAGE"
+exit $EXIT_CODE
